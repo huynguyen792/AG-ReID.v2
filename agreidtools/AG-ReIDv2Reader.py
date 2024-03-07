c2346308524708410744e061254ee70d1c10aae6
Huy Nguyen <nhathuy.ir@gmail.com>
2024-03-07 15:33:58 +1000

    Remove AG-ReIDReader.py file

--- agreidtools/AG-ReIDv2Reader.py
+++ agreidtools/AG-ReIDv2Reader.py
@@ -1,13 +1,13 @@
 import argparse
 
-from datasets import AG_ReID
+from datasets import AG_ReID_v2
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='AG-ReID Data Reader.')
+    parser = argparse.ArgumentParser(description='AG-ReID.v2 Data Reader.')
     parser.add_argument('--data_path', required=True, default='./data')
     args = parser.parse_args()
 
-    dset = AG_ReID(root=args.data_path)
+    dset = AG_ReID_v2(root=args.data_path)
     
